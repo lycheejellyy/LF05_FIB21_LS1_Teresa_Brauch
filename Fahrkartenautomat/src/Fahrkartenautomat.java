9fd8b4c6c6d962e1597ec529e1c1b283aeedd43c
lycheejellyy <t.brauch.02@gmail.com>
2023-01-14 21:01:07 +0100

    A7.4: Das Fahrkartenarray

--- Fahrkartenautomat/src/Fahrkartenautomat.java
+++ Fahrkartenautomat/src/Fahrkartenautomat.java
@@ -28,46 +28,65 @@ class Fahrkartenautomat {
 	public static void begruessung() {
 		System.out.println("Herzlich Willkommen!\n");
 	}
-
+	
 	// Methode f�r Fahrkartenbestellung
 	public static double fahrkartenbestellungErfassen(Scanner tastatur) {
 		double zuZahlenderBetrag = 0;
 		int ticketanzahl;
 		int artticket = 0;
 		String ticketdescr = " ";
-		System.out.println("W�hlen Sie ihre Wunschfahrkarte f�r Berlin AB aus:\r\n"
-				+ "  Kurzstrecke AB [2,00 EUR] (1)\r\n"
-				+ "  Einzelfahrschein AB [3,00 EUR] (2)\r\n"
-				+ "  Tageskarte AB [8,80 EUR] (3)\r\n"
-				+ "  4-Fahrten-Karte AB [9,40 EUR] (4)");
-		artticket = tastatur.nextInt();
+		String[] fahrkartenNamen = {
+				"Einzelfahrschein AB",
+				"Einzelfahrschein BC",
+				"Einzelfahrschein ABC",
+				"Kurzstrecke AB",
+				"Tageskarte AB",
+				"Tageskarte BC",
+				"Tageskarte ABC",
+				"4-Fahrten-Karte AB",
+				"4-Fahrten-Karte BC",
+				"4-Fahrten-Karte ABC",
+				"Kleingruppen-Tageskarte AB",
+				"Kleingruppen-Tageskarte BC",
+				"Kleingruppen-Tageskarte ABC"
+		};
 
-		switch (artticket) {
-			case 1:
-				zuZahlenderBetrag = 2;
-				ticketdescr = "Kurzstrecke AB [2,00 EUR]";
-				break;
-			case 2:
-				zuZahlenderBetrag = 3;
-				ticketdescr = "Einzelfahrschein AB [3,00 EUR]";
-				break;
-			case 3:
-				zuZahlenderBetrag = 8.80;
-				ticketdescr = "Tageskarte AB [8,80 EUR]";
-				break;
-			case 4:
-				zuZahlenderBetrag = 9.40;
-				ticketdescr = "4-Fahrten-Karte AB [9,40 EUR]";
-				break;
-			default:
-				System.out.println("Ung�ltige Angabe! Geben Sie eine Zahl zwischen 1-4 an!");
-				System.out.println("W�hlen Sie ihre Wunschfahrkarte f�r Berlin AB aus:\r\n"
-						+ "  Kurzstrecke AB [2,00 EUR] (1)\r\n"
-						+ "  Einzelfahrschein AB [3,00 EUR] (2)\r\n"
-						+ "  Tageskarte AB [8,80 EUR] (3)\r\n"
-						+ "  4-Fahrten-Karte AB [9,40 EUR] (4)");
-				artticket = tastatur.nextInt();
+		Double[] fahrkartenPreise = {
+				3.00,
+				3.50,
+				3.80,
+				2.00,
+				8.60,
+				9.20,
+				10.00,
+				9.40,
+				12.60,
+				13.80,
+				25.50,
+				26.00,
+				26.50
+		};
+
+		//Ausgabe Auswahlmöglichkeiten
+		System.out.println("W�hlen Sie ihre Wunschfahrkarte f�r Berlin aus: \r\n");
+		for (int i = 0; i < fahrkartenNamen.length; i++) {
+			System.out.print("\n" + fahrkartenNamen[i] + " [" );
+			System.out.printf("%.2f", fahrkartenPreise[i]);
+			System.out.print(" EUR](" + (i + 1) + ")\r\n");
 		}
+
+		//Usereingabe & Überprüfung ob Eingabe valide
+		do {
+			if(artticket < 0 || artticket > fahrkartenNamen.length) {
+				System.out.println("Ungültige Eingabe! Geben Sie eine Zahl zwischen 1-" + fahrkartenNamen.length + " an!");
+			}
+			artticket = tastatur.nextInt();
+		}
+		while (artticket < 0 || artticket > fahrkartenNamen.length);
+
+		ticketdescr = fahrkartenNamen[artticket-1];
+		zuZahlenderBetrag = fahrkartenPreise[artticket-1];
+		
 		// Ausgabe Ticketauswahl
 		System.out.println("Deine Auswahl: " + ticketdescr);
 		// Eingabe Ticketanzahl
@@ -75,7 +94,7 @@ class Fahrkartenautomat {
 		ticketanzahl = tastatur.nextInt();
 		// Ticketeingabe nach falscher Eingabe wiederholen
 		while (ticketanzahl > 10 || ticketanzahl < 1) {
-			System.out.println("Ung�ltige Angabe! Geben Sie eine Zahl zwischen 1-10 an!");
+			System.out.println("Ungültige Angabe! Geben Sie eine Zahl zwischen 1-10 an!");
 			System.out.print("Anzahl der Fahrscheine(max 10): ");
 			ticketanzahl = tastatur.nextInt();
 		}
@@ -94,7 +113,7 @@ class Fahrkartenautomat {
 			System.out.println("Noch zu zahlen: ");
 			System.out.printf("%.2f", nochZuZahlen);
 			System.out.print(" Euro\n");
-			System.out.print("Eingabe (mind. 5 Cent, h�chstens 2 Euro): ");
+			System.out.print("Eingabe in Euro (mind. 5 Cent, h�chstens 20 Euro): ");
 			eingeworfeneMuenze = tastatur.nextDouble();
 			if (eingeworfeneMuenze == 0.05 || eingeworfeneMuenze == 0.10 || eingeworfeneMuenze == 0.20
 					|| eingeworfeneMuenze == 0.50 || eingeworfeneMuenze == 1 || eingeworfeneMuenze == 2
